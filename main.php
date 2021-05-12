d71bf52d2e995bd9ccb3b462c5a72632891a830d
chinatsu.haraikawa <pechi72@icloud.com>
2021-05-12 06:55:33 +0000

    first-commit

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'human.php';
+
+class Main {
+    static function start() {
+        // インスタンスの生成
+        $tanaka = new Human();
+        $suzuki = new Human();
+        $sato = new Human();
+
+        // インスタンスのメソッドを実行
+
+    }
+}
+
+Main::start();
