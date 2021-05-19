406e0f58ba956d23e223a87c751f7616e6c16010
chinatsu.haraikawa <pechi72@icloud.com>
2021-05-19 08:13:32 +0000

    complete

--- human.php
+++ human.php
@@ -1,4 +1,21 @@
-<!--Animal クラスを継承してください。-->
-<!--Thinkable トレイトの think メソッドを使えるように設定してください。-->
-<!--趣味を格納するプロパティを定義してください。-->
-<!--コンストラクタが名前、年齢、趣味の値を引数で受け取り、それらの値で名前、年齢、趣味のプロパティを初期化するようにしてください。引数の名前は自由とします。-->
+<?php
+
+require_once 'animal.php';
+require_once 'thinkable.php';
+
+class Human extends Animal {
+    
+    use Think;
+
+    public $name = '';
+    public $age = '';
+    public $hobby = '';
+    
+    // インスタンスを初期化するための、特別な関数
+    function __construct($name, $age, $hobby) {
+        $this->name = $name;
+        $this->age  = $age;
+        $this->hobby  = $hobby;
+    }
+
+}
